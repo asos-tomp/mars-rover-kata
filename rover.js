a0cb22a8296dbfbaabf57449845c2661f4a47c69
Tom Pereira <tompe@asos.com>
2022-09-25 19:22:46 +0100

    GREEN: unconditional -> conditional. reverse the cardinals based on command.

--- rover.js
+++ rover.js
@@ -2,7 +2,11 @@ export default (input) => {
   const [, state, command] = input.split("\n");
 
   if (command) {
-    const cardinals = ["N", "E", "S", "W"];
+    let cardinals = ["N", "E", "S", "W"];
+    if (command === "L") {
+      cardinals.reverse();
+    }
+
     const startOrientation = state.slice(-1);
     const startOrientationIndex = cardinals.indexOf(startOrientation);
     const newOrientation =
