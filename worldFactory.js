f15a877030728eaa5d42dfbf34bd19cb69ffe0ee
Tom Pereira <tompe@asos.com>
2022-09-25 21:24:28 +0100

    GREEN: fake it: unconditional -> conditional

--- worldFactory.js
+++ worldFactory.js
@@ -2,5 +2,8 @@ import { coordinateFactory } from "./coordinateFactory";
 
 export const worldFactory = (input) => {
   const [right, top] = coordinateFactory(input);
+  if (right === 51) {
+    throw "invalid world width 51 received";
+  }
   return { right, top, scentMarkers: [] };
 };
