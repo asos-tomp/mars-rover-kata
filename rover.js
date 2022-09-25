4d120650f1dda80f3fabc69fa3fc3126f6827eeb
Tom Pereira <tompe@asos.com>
2022-09-25 19:46:45 +0100

    GREEN: if -> while. allow multiple rotations.

--- rover.js
+++ rover.js
@@ -2,21 +2,22 @@ export default (input) => {
   const [, state, instructions] = input.split("\n");
 
   if (instructions) {
-    let cardinals = ["N", "E", "S", "W"];
-    if (["LR", "RL"].includes(instructions)) {
-      return state;
-    }
-
-    if (instructions.startsWith("L")) {
-      cardinals.reverse();
-    }
+    const cardinals = ["N", "E", "S", "W"];
 
     const startOrientation = state.slice(-1);
-    const startOrientationIndex = cardinals.indexOf(startOrientation);
-    const rotations = instructions.length;
-
-    const newOrientation =
-      cardinals[(startOrientationIndex + rotations) % cardinals.length];
+    let orientationIndex = cardinals.indexOf(startOrientation);
+    const instructionDeltaMap = {
+      L: -1,
+      R: 1,
+    };
+    const modulo = (n, m) => ((n % m) + m) % m;
+    instructions.split("").forEach((instruction) => {
+      orientationIndex = modulo(
+        orientationIndex + instructionDeltaMap[instruction],
+        4
+      );
+    });
+    const newOrientation = cardinals[orientationIndex];
 
     return `0 0 ${newOrientation}`;
   }
