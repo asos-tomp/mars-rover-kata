827ab01160176440e9e44b82c157ff0dbacdbeb5
Tom Pereira <tompe@asos.com>
2022-09-25 21:35:53 +0100

    GREEN: align to correct terminology

--- worldFactory.js
+++ worldFactory.js
@@ -3,7 +3,7 @@ import { coordinateFactory } from "./coordinateFactory";
 export const worldFactory = (input) => {
   const [right, top] = coordinateFactory(input);
   if (right < 0 || right > 50) {
-    throw `invalid world width ${right} received`;
+    throw `invalid world right coordinate ${right} received`;
   }
 
   return { right, top, scentMarkers: [] };
