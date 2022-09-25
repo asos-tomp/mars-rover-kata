189b1f7d92c61dabff5b91febc855f3fecfcd1a3
Tom Pereira <tompe@asos.com>
2022-09-25 21:27:55 +0100

    GREEN: cater for boundaries of invalid widths

--- worldFactory.js
+++ worldFactory.js
@@ -2,8 +2,9 @@ import { coordinateFactory } from "./coordinateFactory";
 
 export const worldFactory = (input) => {
   const [right, top] = coordinateFactory(input);
-  if (right === 51) {
-    throw "invalid world width 51 received";
+  if (right < 0 || right > 50) {
+    throw `invalid world width ${right} received`;
   }
+
   return { right, top, scentMarkers: [] };
 };
