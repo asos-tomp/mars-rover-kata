23efead365aa0ea1adc9ae44c9af89f9ff07ebdc
Tom Pereira <tompe@asos.com>
2022-09-25 21:23:59 +0100

    RED: handle boundary condition for invalid world width

--- rover.test.js
+++ rover.test.js
@@ -12,114 +12,105 @@ describe("Mars Rover", () => {
   describe("given a single robot ", () => {
     const origin = "0 0";
 
-    describe("with a valid start location (0 0) and a minimum viable world (single unit size)", () => {
+    describe("with a valid start location (0 0)", () => {
       const location = `${origin}`;
-      const world = `${origin}`;
 
-      describe.each`
-        orientation
-        ${"N"}
-        ${"E"}
-        ${"S"}
-        ${"W"}
-      `("and orientation cardinal ($orientation)", ({ orientation }) => {
-        const state = `${location} ${orientation}`;
-
-        describe("and no instructions", () => {
-          it(`should return the starting state ${state} unchanged`, () => {
-            expect(rover(`${world}\n${state}`)).toEqual(state);
-          });
-        });
+      describe("and a minimum viable world (single unit size)", () => {
+        const world = `${origin}`;
 
         describe.each`
-          instructions
-          ${"R"}
-          ${"L"}
-          ${"RR"}
-          ${"LL"}
-          ${"RRR"}
-          ${"LLL"}
-          ${"LR"}
-          ${"RL"}
-          ${"RLR"}
-        `("and instructions ($instructions)", ({ instructions }) => {
-          const cardinals = ["N", "E", "S", "W"];
-          let orientationIndex = cardinals.indexOf(orientation);
+          orientation
+          ${"N"}
+          ${"E"}
+          ${"S"}
+          ${"W"}
+        `("and orientation cardinal ($orientation)", ({ orientation }) => {
+          const state = `${location} ${orientation}`;
 
-          instructions.split("").forEach((instruction) => {
-            const instructionDeltaMap = {
-              L: -1,
-              R: 1,
-            };
-            const modulo = (n, m) => ((n % m) + m) % m;
-            orientationIndex = modulo(
-              orientationIndex + instructionDeltaMap[instruction],
-              4
-            );
+          describe("and no instructions", () => {
+            it(`should return the starting state ${state} unchanged`, () => {
+              expect(rover(`${world}\n${state}`)).toEqual(state);
+            });
           });
 
-          const expectedOrientation = cardinals[orientationIndex];
-          it(`should return the starting location with an updated orientation (${expectedOrientation})`, () => {
-            expect(rover(`${world}\n${state}\n${instructions}`)).toEqual(
-              `${location} ${expectedOrientation}`
-            );
+          describe.each`
+            instructions
+            ${"R"}
+            ${"L"}
+            ${"RR"}
+            ${"LL"}
+            ${"RRR"}
+            ${"LLL"}
+            ${"LR"}
+            ${"RL"}
+            ${"RLR"}
+          `("and rotation instructions ($instructions)", ({ instructions }) => {
+            const cardinals = ["N", "E", "S", "W"];
+            let orientationIndex = cardinals.indexOf(orientation);
+
+            instructions.split("").forEach((instruction) => {
+              const instructionDeltaMap = {
+                L: -1,
+                R: 1,
+              };
+              const mod = (n, m) => ((n % m) + m) % m;
+              orientationIndex = mod(
+                orientationIndex + instructionDeltaMap[instruction],
+                4
+              );
+            });
+
+            const expectedOrientation = cardinals[orientationIndex];
+
+            it(`should return the starting location with an updated orientation (${expectedOrientation})`, () => {
+              expect(rover(`${world}\n${state}\n${instructions}`)).toEqual(
+                `${location} ${expectedOrientation}`
+              );
+            });
           });
-        });
 
-        describe("and a forward instruction (F)", () => {
-          const instruction = "F";
+          describe("and a forward instruction (F)", () => {
+            const instruction = "F";
 
-          it(`should return the starting location with an indication that the robot is lost`, () => {
-            expect(rover(`${world}\n${state}\n${instruction}`)).toEqual(
-              `${state} LOST`
-            );
+            it(`should return the starting location with an indication that the robot is lost`, () => {
+              expect(rover(`${world}\n${state}\n${instruction}`)).toEqual(
+                `${state} LOST`
+              );
+            });
           });
         });
       });
-    });
 
-    describe("and a forward instruction", () => {
-      const instruction = "F";
+      describe("and an invalid world width (51 0)", () => {
+        const width = 51;
+        const world = `${width} 0`;
+        const state = `${location} N`;
 
-      describe.each`
-        world    | location | orientation | expectedLocation
-        ${"0 1"} | ${"0 0"} | ${"N"}      | ${"0 1"}
-        ${"0 1"} | ${"0 1"} | ${"S"}      | ${"0 0"}
-        ${"1 0"} | ${"0 0"} | ${"E"}      | ${"1 0"}
-        ${"1 0"} | ${"1 0"} | ${"W"}      | ${"0 0"}
-        ${"0 2"} | ${"0 2"} | ${"S"}      | ${"0 1"}
-        ${"3 0"} | ${"3 0"} | ${"W"}      | ${"2 0"}
-      `(
-        "and a world with top right coordinate of ($world), a valid start location ($location) and orientation cardinal ($orientation)",
-        ({ world, location, orientation, expectedLocation }) => {
-          const state = `${location} ${orientation}`;
+        it(`should throw an exception indicating that the coordinate is outside of accepted bounds`, () => {
+          expect(() => rover(`${world}\n${state}`)).toThrow(
+            `invalid world width ${width} received`
+          );
+        });
+      });
 
-          it(`should return a new state (${expectedLocation} ${orientation}) and no indication that the robot is lost`, () => {
-            expect(rover(`${world}\n${state}\n${instruction}`)).toEqual(
-              `${expectedLocation} ${orientation}`
-            );
-          });
-        }
-      );
-    });
+      describe("and a world with with top right coordinate of (5 3)", () => {
+        const world = "5 3";
 
-    describe("and a world with with top right coordinate of (5 3)", () => {
-      const world = "5 3";
-
-      describe.each`
-        state      | instructions       | expectation
-        ${"1 1 E"} | ${"RFRFRFRF"}      | ${"1 1 E"}
-        ${"3 2 N"} | ${"FRRFLLFFRRFLL"} | ${"3 3 N LOST"}
-      `(
-        "and a valid start state ($state) and instructions ($instructions)",
-        ({ state, instructions, expectation }) => {
-          it(`should return a new state (${expectation})`, () => {
-            expect(rover(`${world}\n${state}\n${instructions}`)).toEqual(
-              expectation
-            );
-          });
-        }
-      );
+        describe.each`
+          state      | instructions       | expectation
+          ${"1 1 E"} | ${"RFRFRFRF"}      | ${"1 1 E"}
+          ${"3 2 N"} | ${"FRRFLLFFRRFLL"} | ${"3 3 N LOST"}
+        `(
+          "and a valid start state ($state) and instructions ($instructions)",
+          ({ state, instructions, expectation }) => {
+            it(`should return a new state (${expectation})`, () => {
+              expect(rover(`${world}\n${state}\n${instructions}`)).toEqual(
+                expectation
+              );
+            });
+          }
+        );
+      });
     });
   });
 
