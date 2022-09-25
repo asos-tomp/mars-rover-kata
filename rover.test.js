d52dc236761bff27ba612a5d3daeaa33abc1733b
Tom Pereira <tompe@asos.com>
2022-09-25 19:15:05 +0100

    RED: right rotation instruction from N orientation

--- rover.test.js
+++ rover.test.js
@@ -16,18 +16,30 @@ describe("Mars Rover", () => {
       ${"E"}
       ${"S"}
       ${"W"}
-    `(
-      "and orientation cardinal ($orientation), and no instructions",
-      ({ orientation }) => {
-        const origin = "0 0";
-        const world = `${origin}`;
-        const location = `${origin}`;
-        const state = `${location} ${orientation}`;
+    `("and orientation cardinal ($orientation)", ({ orientation }) => {
+      const origin = "0 0";
+      const world = `${origin}`;
+      const location = `${origin}`;
+      const state = `${location} ${orientation}`;
 
-        it("should return the starting state unchanged", () => {
-          expect(rover(`${world}\n${state}`)).toEqual(state);
-        });
-      }
-    );
+      it("should return the starting state unchanged", () => {
+        expect(rover(`${world}\n${state}`)).toEqual(state);
+      });
+    });
+
+    describe("given a single robot, a minimum viable world (single unit size), a valid start location (0 0) and orientation cardinal (N) and a right rotation instruction", () => {
+      const orientation = "N";
+      const origin = "0 0";
+      const world = `${origin}`;
+      const location = `${origin}`;
+      const state = `${location} ${orientation}`;
+      const instruction = "R";
+
+      it(`should return the location with an updated orientation (E)`, () => {
+        expect(rover(`${world}\n${state}\n${instruction}`)).toEqual(
+          `${location} E`
+        );
+      });
+    });
   });
 });
