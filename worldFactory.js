ee4020bb36d1547b6c8266c97bb6dfdf724d594f
Tom Pereira <tompe@asos.com>
2022-09-25 21:38:10 +0100

    GREEN: handle boundary conditions of world setup

--- worldFactory.js
+++ worldFactory.js
@@ -1,9 +1,12 @@
 import { coordinateFactory } from "./coordinateFactory";
 
 export const worldFactory = (input) => {
-  const [right, top] = coordinateFactory(input);
-  if (right < 0 || right > 50) {
-    throw `invalid world right coordinate ${right} received`;
+  const coords = coordinateFactory(input);
+  const [right, top] = coords;
+  const isInvalid = (coord) => coord > 50 || coord < 0;
+
+  if (coords.some(isInvalid)) {
+    throw `invalid world right top coordinate (${right} ${top}) received`;
   }
 
   return { right, top, scentMarkers: [] };
