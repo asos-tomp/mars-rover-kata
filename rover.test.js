38a5b8b62cdb5613aa9c0b24d61f22539c779225
Tom Pereira <tompe@asos.com>
2022-09-25 19:06:42 +0100

    RED: mvp world & robot, no instructions. should parrot state back.

--- rover.test.js
+++ rover.test.js
@@ -8,4 +8,16 @@ describe("Mars Rover", () => {
   it("should export a function as a default export", () => {
     expect(rover).toBeInstanceOf(Function);
   });
+
+  describe("given a minimum viable world (single unit size), a single robot with a valid start location (0,0) and orientation cardinal (N), and no moves", () => {
+    const origin = "0 0";
+    const world = `${origin}`;
+    const location = `${origin}`;
+    const orientation = "N";
+
+    it("should return the starting state unchanged", () => {
+      const state = `${location} ${orientation}`;
+      expect(rover(`${world}\n${state}`)).toEqual(state);
+    });
+  });
 });
